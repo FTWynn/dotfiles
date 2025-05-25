f7182d0011382374385cdc4a57093c54ddbb7112
David Wynn <Remixer96@gmail.com>
2025-05-24 21:21:17 -0400

    sprucing up fish again with bash learnings included now

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,21 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set fish_greeting
-    starship init fish | source
-    #atuin init fish | source
-    direnv hook fish | source
+	# Commands to run in interactive sessions can go here
+	set fish_greeting
+	starship init fish | source
+	#atuin init fish | source
+	direnv hook fish | source
 
-    # Aliases, also known as abbreviations
-    abbr l eza
-    abbr ll eza --group --header --group-directories-first --long --icons=always --all
+	# Aliases, also known as abbreviations
+	abbr l eza
+	abbr ll eza --group --header --group-directories-first --long --icons=always --all --time-style=long-iso
+
+	# Nix exports and abbreviations
+	set -gx FLAKE "/home/david/dotfiles/nix"
+
+	abbr nhs 'nh os switch -- --impure --option substitutors ''https://cache.nixos.org'''
+	abbr nhu 'nh os switch -u -- --impure --option substitutors ''https://cache.nixos.org'''
+	abbr nhc 'nh os clean all --keep 5'
+
+	# Setup zoxide
+	zoxide init fish | source
 end
