0fa7ffec56ada26512d2bad43303892a67e8b48f
David Wynn <Remixer96@gmail.com>
2025-06-06 06:56:26 -0400

    add function for yazi cd'ing on exit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,4 +19,13 @@ if status is-interactive
 
 	# Setup zoxide
 	zoxide init fish | source
+
+	function y
+		set tmp (mktemp -t "yazi-cwd.XXXXXX")
+		yazi $argv --cwd-file="$tmp"
+		if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		   builtin cd -- "$cwd"
+		end
+		rm -f -- "$tmp"
+	end
 end
